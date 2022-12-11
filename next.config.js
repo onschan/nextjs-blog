403c10752400ec2d1f11d56b434a64c239215bfe
cks3066 <cks3066@naver.com>
2022-12-11 16:17:55 +0900

    chore: set project config with emotion

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
