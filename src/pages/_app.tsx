ecd77657bb1f7a2db68b687a03d6c17f3a6eadb3
cks3066 <cks3066@naver.com>
2024-12-10 00:00:34 +0900

    feat: url 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Global styles={globalStyle} />
       <ThemeModeScript />
+      <SEO title="온승찬 블로그" description="안녕하세요" url="/" />
       <Header />
       <main>
         <Component {...pageProps} />
